36faadd1578a4ad6141bf5bd8070d03053a2a488
valenlzmj <64485118+valenlzmj@users.noreply.github.com>
2021-11-16 18:21:28 -0500

    ESTE SI

--- pagina/src/components/Goal.jsx
+++ pagina/src/components/Goal.jsx
@@ -20,7 +20,7 @@ const Goal = () => {
             <div className="part">
                 <img src={rulesI} alt="Reglas" />
                 <div className="text">
-                    <h3>OBJETIVOS</h3>
+                    <h3>REGLAS</h3>
                     <p>El personaje solo se puede mover por el
                         camino.<br /><br />
                         Se debe llegar al final del camino
@@ -30,7 +30,7 @@ const Goal = () => {
             <div className="part">
                 <img src={rewardsI} alt="Recompensas" />
                 <div className="text">
-                    <h3>OBJETIVOS</h3>
+                    <h3>RECOMPENSAS</h3>
                     <p>Mover de forma correcta el mundo,
                         completa el camino.<br /><br />
                         Al conseguir todas las galletas de un
