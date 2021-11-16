4e05a789ba5f8288820f53218ad07405454a1f61
Alejoocv <70666037+Alejoocv@users.noreply.github.com>
2021-11-16 15:49:14 -0500

    el enviar al correo

--- pagina/src/pages/Team.jsx
+++ pagina/src/pages/Team.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Equipo from '../components/equipo';
+import Unete from '../components/unete';
 import './styles/Experience.scss';
 
 const Team = () => {
     return(
         <main className="Team">
           
-                      <div className="containerExperience">
-                <h1>EXPERIENCIA DEL JUEGO</h1>
+           <div className="containerExperience">
+                <h1>EQUIPO DE TRABAJO</h1>
             </div>  
           <section className="equipo__section">
                 <Equipo />
           </section> 
-
+          <div className="containerExperience">
+          <section className="unete__section">
+          
+            <h2> Únete al equipo, envíanos un mail</h2>
+                <Unete />
+          </section> 
+</div>
         </main>
     )
 }
