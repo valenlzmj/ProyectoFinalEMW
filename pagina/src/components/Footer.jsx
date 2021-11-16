6792b83fc9e8c898f77198abf3fd6fa3ab49c4e0
valenlzmj <64485118+valenlzmj@users.noreply.github.com>
2021-11-16 17:19:45 -0500

    cambios

--- pagina/src/components/Footer.jsx
+++ pagina/src/components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
                     <div className="row">
                         <p className="col-sm">
                             {new Date().getFullYear()} |
-                            <a href="mailto:alejandro.cruz_vil@uao.edu.co">alejandro.cruz_vil@uao.edu.co </a> | 
-                            <a href="mailto:valentina.loaiza@uao.edu.co">valentina.loaiza@uao.edu.co </a> |  
-                            <a href="mailto:andrea.saavedra@uao.edu.co">andrea.saavedra@uao.edu.co </a> | 
-                            <a href="mailto:andres_fel.calero@uao.edu.co">andres_fel.calero@uao.edu.co </a> 
+                            <a href="mailto:alejandro.cruz_vil@uao.edu.co">  alejandro.cruz_vil@uao.edu.co </a> | 
+                            <a href="mailto:valentina.loaiza@uao.edu.co">  valentina.loaiza@uao.edu.co </a> |  
+                            <a href="mailto:andrea.saavedra@uao.edu.co">  andrea.saavedra@uao.edu.co </a> | 
+                            <a href="mailto:andres_fel.calero@uao.edu.co">  andres_fel.calero@uao.edu.co </a> 
                         </p>
                     </div>
                 </div>
